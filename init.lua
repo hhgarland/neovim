1d30732158be05e1b43abd00e57cbda9659cedc2
Hayden G <hhgarland@proton.me>
2023-01-06 06:08:05 -0600

    Plugin changes

--- init.lua
+++ init.lua
@@ -47,11 +47,13 @@ require('packer').startup(function(use)
 
   -- Other plugins
   use 'gruvbox-community/gruvbox' -- Gruvbox theme
+  use {"rockyzhang24/arctic.nvim", requires = {"rktjmp/lush.nvim"}}
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
   use 'tpope/vim-surround' -- Surround provides mappings to easily delete, change and add such surroundings in pairs
+  use 'dhruvasagar/vim-table-mode'
 
 use { -- Fancier tabline
   'kdheepak/tabline.nvim',
